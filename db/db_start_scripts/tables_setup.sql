6ead2eab93f2ac8d1957f2a3535999c46235fc8d
jetilling <jetilling@hotmail.com>
2017-09-09 10:50:10 -0600

    Added tables to database

--- db/db_start_scripts/tables_setup.sql
+++ db/db_start_scripts/tables_setup.sql
@@ -18,3 +18,53 @@ CREATE TABLE statuses (
   status TEXT,
   created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 );
+
+-- Create goals table
+CREATE TABLE goals (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  goal TEXT,
+  created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  reminder TIMESTAMP,
+  completed BOOLEAN,
+  challenge_level INTEGER,
+  fulfillment_level INTEGER
+);
+
+-- Create tags table
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  tag TEXT
+);
+
+-- Create tomorrow_thoughts table
+CREATE TABLE tomorrow_thoughts (
+  id SERIAL PRIMARY KEY,
+  thought TEXT,
+  created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  appear_on TIMESTAMP
+);
+
+-- Create relations_to_tags table
+CREATE TABLE relations_to_tag (
+  id SERIAL PRIMARY KEY,
+  goal_id INTEGER REFERENCES goals(id),
+  tags_id INTEGER REFERENCES tags(id),
+  users_id INTEGER REFERENCES users(id)
+);
+
+-- Create relations_to_completion_tags table
+CREATE TABLE relations_to_completion_tags (
+  id SERIAL PRIMARY KEY,
+  goal_id INTEGER REFERENCES goals(id),
+  tags_id INTEGER REFERENCES tags(id),
+  users_id INTEGER REFERENCES users(id)
+);
+
+-- Create relations_to_tomorrow_thoughts table
+CREATE TABLE relations_to_tomorrow_thoughts (
+  id SERIAL PRIMARY KEY,
+  goal_id INTEGER REFERENCES goals(id),
+  tomorrow_thoughts_id INTEGER REFERENCES tomorrow_thoughts(id),
+  users_id INTEGER REFERENCES users(id)
+);
